e71fd014a21fe3859a7685e68664887d80fac5e2
Elizabeth Dewey <edewey@bellevue.edu>
2020-05-29 09:55:53 -0500

    adding my primitives assignment

---
+++ Dewey-PrimitiveTypes.js
@@ -0,0 +1,25 @@
+
+var firstName;
+firstName = 'Elizabeth';
+
+var lastName;
+lastName = 'Dewey';
+
+var myAge;
+myAge = '37';
+
+var randomStreet;
+randomStreet = '123 ABC Lane';
+
+var randomCity;
+randomCity = 'Alphabet City';
+
+var zipCode;
+zipCode = '12345';
+
+console.log(firstName);
+console.log(lastName);
+console.log(myAge);
+console.log(randomStreet);
+console.log(randomCity);
+console.log(zipCode);
