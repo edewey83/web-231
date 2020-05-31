71b2c0fe42002217a563c432bdc8ee175dcf27d4
Elizabeth Dewey <edewey@bellevue.edu>
2020-05-31 12:13:40 -0500

    Adding debugged code for discussion

---
+++ Tiffany-Baker.js
@@ -0,0 +1,92 @@
+  /* 
+    
+ 
+    
+ 
+     ============================================ 
+    
+ 
+    
+ 
+     ; Title: discussion 1.1 
+    
+ 
+    
+ 
+     ; Author: Tiffany Baker 
+    
+ 
+    
+ 
+     ; Date: 30 May 2020 
+    
+ 
+    
+ 
+     ; Modified By: Tiffany Baker 
+    
+ 
+    
+ 
+     ; Description: This program uses javascript variables to create a line of text. 
+    
+ 
+    
+ 
+     ;=========================================== 
+    
+ 
+    
+ 
+     */ 
+       
+    
+ 
+    
+ 
+    
+ 
+     var 
+       
+     greeting 
+      =  
+     "Hello There!" 
+     ; 
+    
+ 
+    
+ 
+     var 
+       
+     question 
+      =  
+     " How are you today?" 
+     ; 
+    
+ 
+    
+ 
+     var 
+       
+     text 
+      =  
+     greeting //should be greeting not greet. Corrected the variable. 
+      +  
+     question 
+     ;  
+     //this variable creates a line of text from both previous variables. 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+    
+ 
+     console.log // should have been console.log not console log, was missing the period
+      ( 
+     text 
+     ); 
+    
